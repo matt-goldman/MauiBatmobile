f6f4c9d74433575a79ecfdaca13da37e2865c409
Matt Goldman <matt.goldman@outlook.com>
2022-03-29 08:47:59 +1100

    added server

---
+++ Services/RpmService.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace MauiBatmobile.Services;
+
+public class RpmService : Rpm.RpmBase
+{
+    public int LatestRpm { get; set; }
+
+    public override async Task<LogResponse> SendRpm(IAsyncStreamReader<LogRequest> requestStream, ServerCallContext context)
+    {
+        await foreach (var request in requestStream.ReadAllAsync())
+        {
+            LatestRpm = request.Rpm;
+            Console.WriteLine($"Batmobile RPM: {request.Rpm}");
+        }
+
+        Console.WriteLine("Batmobile connection terminated");
+    }
+
+    public override async Task GetRpm(Empty _, IServerStreamWriter<LogRequest> serverStream, ServerCallContext context)
+    {
+        while(!context.CancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(100);
+
+            Console.WriteLine($"Sending RPM to Batcave: {LatestRpm}");
+
+            await serverStream.WriteAsync(new LogRequest { Rpm = LatestRpm });
+        }
+
+        Console.WriteLine("Batcave connection terminated");
+    }
+}
