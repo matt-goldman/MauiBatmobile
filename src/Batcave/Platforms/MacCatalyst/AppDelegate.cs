7755af6639b01022b007450d5cc6e13322abd82f
Matt Goldman <matt.goldman@outlook.com>
2022-03-29 09:03:32 +1100

    Updated project structure

---
+++ src/Batcave/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Batcave
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
