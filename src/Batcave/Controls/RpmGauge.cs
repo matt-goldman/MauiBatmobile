88415df3a2646b204d200448272a278aec1038e9
Matt Goldman <matt.goldman@outlook.com>
2022-03-31 10:55:51 +1100

    Hooked up throttle properties Started adding gauge

---
+++ src/Batcave/Controls/RpmGauge.cs
@@ -0,0 +1,29 @@
+﻿namespace Batcave.Controls;
+
+public class RpmGauge : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeSize = 4;
+
+        canvas.StrokeColor = Colors.Red;
+        var path = new PathF();
+        path.MoveTo(0, 100);
+        path.LineTo(200, 100);
+        path.Close();
+
+        canvas.DrawPath(path);
+
+        var bluePath = new PathF();
+        canvas.StrokeColor = Colors.Blue;
+        bluePath.MoveTo(100, 105);
+        bluePath.LineTo(200, 105);
+        bluePath.LineTo(200, 105);
+        bluePath.Close();
+
+        canvas.DrawPath(bluePath); 
+
+        canvas.StrokeColor = Colors.Black;
+        canvas.DrawArc(0, 0, 200, 200, 0, 180, false, false);        
+    }
+}
--- src/Batmobile/Controls/Throttle.xaml.cs
+++ src/Batmobile/Controls/Throttle.xaml.cs
@@ -2,14 +2,32 @@ namespace Batmobile.Controls;
 
 public partial class Throttle : ContentView
 {
-	public Throttle()
+    public static BindableProperty RpmProperty = BindableProperty.Create(nameof(Rpm), typeof(int), typeof(Throttle), null);
+    public int Rpm
+    {
+        get { return (int)GetValue(RpmProperty); }
+        set { SetValue(RpmProperty, value); }
+    }
+
+    public Throttle()
 	{
 		InitializeComponent();
 	}
 
     private void ThrottlePanUpdated(object sender, PanUpdatedEventArgs e)
     {
-        if (e.TotalY > 0 && e.TotalY < 300)
+        if (e.TotalY > 0 && e.TotalY < 300 && IsEnabled)
+        {
             handle.TranslationY = e.TotalY;
+
+            // Total RPM will be a fraction of 15,000 (max)
+            // TotalY will be a number between 0 and 300
+            // Set Rpm to TotalY * 50 to give the fraction of
+            // max RPM set by this translation
+            Rpm = (int)e.TotalY * 50;
+        }
+
+        if (e.StatusType == GestureStatus.Completed || e.StatusType == GestureStatus.Canceled)
+            Rpm = 0;
     }
 }
